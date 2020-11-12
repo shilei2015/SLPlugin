7c731f40020a41c60a9e7a19646dbe4bfad8cd6d
shilei2015 <244235126@qq.com>
2020-11-12 15:31:24 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-SLPlugin_Example/Pods-SLPlugin_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SLPlugin_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SLPlugin_Example
+@end
---
+++ Example/Pods/Target Support Files/SLPlugin/SLPlugin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SLPlugin : NSObject
+@end
+@implementation PodsDummy_SLPlugin
+@end
